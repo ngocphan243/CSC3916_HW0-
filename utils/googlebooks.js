c6ef194919672b1260cfa19a613588cea35cb620
ngocphan243 <98306356+ngocphan243@users.noreply.github.com>
2022-01-30 19:13:46 -0700

    Update googlebooks.js

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -8,14 +8,21 @@ module.exports = async (phrase) => {
         }
     })
 
-    return JSON.stringify(results.data);
+    //return JSON.stringify(results.data);
+    let customObject = {
+        data: results.data,  
+        status: results.status,  
+        statusText: results.statusText,  
+        headers: results.headers, 
+        requestHeader: results.config.headers 
+    }
+    return customObject
 }
 
 /*  console.log(results.data);
     console.log(results.status);
     console.log(results.statusText);
     console.log(results.headers);
-
         const results = await axios({
         method: 'get',
         url: 'https://www.googleapis.com/books/v1/volumes',
